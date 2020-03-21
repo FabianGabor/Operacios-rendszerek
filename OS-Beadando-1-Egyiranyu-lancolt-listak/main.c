2e975d0e05fafff802c74274baf38b1a43e5938c
Fábián Gábor <info@fabiangabor.com>
2020-03-21 17:05:27 +0100

    Insert into list

--- OS-Beadando-1-Egyiranyu-lancolt-listak/main.c
+++ OS-Beadando-1-Egyiranyu-lancolt-listak/main.c
@@ -1,7 +1,31 @@
+/*
+ * Készítsen C++ vagy C# „console” alkalmazást, amely szöveges állományból beolvas 10 darab számot 0,1,2,…,9 és összefűzi azokat egyirányú láncba.
+ * A program képes legyen beszúrni új elemeket, illetve törölni létezőket. Minden művelet végrehajtása után a teljes módosított lista legyen megjelenítve.
+ */
+
 #include <stdio.h>
+
+typedef unsigned int data;
+
+typedef struct node {
+    data data;
+    struct node *next;
+} node;
+
+void insert (node *previous_node, data data) {
+    if (previous_node == NULL) {
+        printf("Previous node was NULL!");
+    }
+
+    node *new_node = (node*) malloc(sizeof(node));
+    new_node->data = data;
+    new_node->next = previous_node->next;
+    previous_node->next = new_node;
+}
+
 
 int main()
 {
-    printf("Hello World!\n");
+
     return 0;
 }
